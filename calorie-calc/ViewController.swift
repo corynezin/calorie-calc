985d001bb9bb78a5121677453e8e06e0f17deca1
Cory Nezin <cory.nezin@gmail.com>
2019-01-01 15:31:26 -0500

    Initial Commit

---
+++ calorie-calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calorie-calc
+//
+//  Created by cnezin on 1/1/19.
+//  Copyright © 2019 Debory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
