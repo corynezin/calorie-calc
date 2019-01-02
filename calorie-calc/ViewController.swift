971b57be479f1e16f48795e0ebb5bf6492d4f358
Cory Nezin <cory.nezin@gmail.com>
2019-01-01 23:42:34 -0500

    Added undo button

--- calorie-calc/ViewController.swift
+++ calorie-calc/ViewController.swift
@@ -11,14 +11,29 @@ import UIKit
 class ViewController: UIViewController {
     
     var calorieCountInteger = 0
-    
+    var calorieList:[Int] = []
+        
     @IBOutlet weak var calorieCount: UILabel!
     @IBOutlet weak var calorieInput: UITextField!
+    
+    func isAppAlreadyLaunchedOnce()->Bool{
+        let defaults = UserDefaults.standard
+        
+        if let isAppAlreadyLaunchedOnce = defaults.string(forKey: "isAppAlreadyLaunchedOnce"){
+            print("App already launched : \(isAppAlreadyLaunchedOnce)")
+            return true
+        }else{
+            defaults.set(true, forKey: "isAppAlreadyLaunchedOnce")
+            print("App launched first time")
+            return false
+        }
+    }
     
     @IBAction func subtract(_ sender: Any) {
         if !calorieInput.text!.isEmpty {
             let calorieInputInt = Int(calorieInput.text!)
             calorieCountInteger -= calorieInputInt!
+            calorieList.append(-calorieInputInt!)
             updateCalories()
         }
     }
@@ -26,7 +41,16 @@ class ViewController: UIViewController {
     @IBAction func add(_ sender: Any) {
         if !calorieInput.text!.isEmpty {
             let calorieInputInt = Int(calorieInput.text!)
+            calorieList.append(calorieInputInt!)
             calorieCountInteger += calorieInputInt!
+            updateCalories()
+        }
+    }
+    
+    @IBAction func undo(_ sender: Any) {
+        if !calorieList.isEmpty {
+            let undoAmount = calorieList.popLast()
+            calorieCountInteger -= undoAmount!
             updateCalories()
         }
     }
