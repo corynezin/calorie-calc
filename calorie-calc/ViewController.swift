47bbcf0a7613aa97860ceb449fc6e931b2036346
Cory Nezin <cory.nezin@gmail.com>
2019-01-01 20:51:48 -0500

    Created a basic prototype

--- calorie-calc/ViewController.swift
+++ calorie-calc/ViewController.swift
@@ -9,7 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var calorieCountInteger = 0
+    
+    @IBOutlet weak var calorieCount: UILabel!
+    @IBOutlet weak var calorieInput: UITextField!
+    
+    @IBAction func subtract(_ sender: Any) {
+        if !calorieInput.text!.isEmpty {
+            let calorieInputInt = Int(calorieInput.text!)
+            calorieCountInteger -= calorieInputInt!
+            updateCalories()
+        }
+    }
+    
+    @IBAction func add(_ sender: Any) {
+        if !calorieInput.text!.isEmpty {
+            let calorieInputInt = Int(calorieInput.text!)
+            calorieCountInteger += calorieInputInt!
+            updateCalories()
+        }
+    }
+    
+    func updateCalories() {
+        calorieInput.text = ""
+        calorieCount.text = String(calorieCountInteger)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Cory was here
@@ -18,7 +44,6 @@ class ViewController: UIViewController {
         // another one
         // Do any additional setup after loading the view, typically from a nib.
     }
-
 
 }
 
